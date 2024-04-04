ac5b529d09f621ad4d556b8dc336bec37d6d4a32
CristeaNicolae <nicolae.cristea2002.pnn@gmail.com>
2024-04-05 00:04:51 +0300

    fin vers

---
+++ ServerClient/Client/Client.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    private string Nume;
+    private string IP;
+    public static void Main()
+    {
+        Console.Write("Nume: ");
+        string Nume = Console.ReadLine() + "";
+
+        Client clientDetails = new Client(Nume);
+
+    reconnect:
+
+        try
+        {
+            TcpClient client = new TcpClient("127.0.0.1", 8080);
+            NetworkStream stream = client.GetStream();
+
+            Console.WriteLine("Conectat la server cu succes :)");
+
+            Thread readThread = new Thread(() => ReadMessageFromServer(stream));
+            readThread.Start();
+
+            //Thread readClient = new Thread(() => ReadMessageFromClient(stream));
+            //readClient.Start();
+
+            while (true)
+            {
+                Console.Write("Introduceti mesajul: ");
+                string message = Console.ReadLine();
+                byte[] data = Encoding.ASCII.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+
+                if (message.Equals("#exit"))
+                {
+                    Environment.Exit(0);
+                }
+
+            }
+
+            //client.Close();
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("Deconectat de la server :(");
+            goto reconnect;
+            //string readmsg = Console.ReadLine() + "";
+            //if (readmsg.Equals("Y") || readmsg.Equals("y")) Environment.Exit(0);
+        }
+        catch (SocketException)
+        {
+            Console.WriteLine("Nu s-a putut conecta la server");
+            while (true)
+            {
+                Console.WriteLine("Reconnect? Y/N");
+                string readmsg = Console.ReadLine() + "";
+                if (readmsg.Equals("N") || readmsg.Equals("n")) Environment.Exit(0);
+                else if (readmsg.Equals("Y") || readmsg.Equals("y")) goto reconnect; 
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("A aparut o eroare" + e.StackTrace);
+        }
+    }
+
+    public static void ReadMessageFromServer(NetworkStream stream)
+    {
+        try
+        {
+            while (true)
+            {
+                byte[] data = new byte[256];
+                int bytes = stream.Read(data, 0, data.Length);
+                string message = Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("\nServer: " + message + "\n");
+                Console.Write("Introduceti mesajul: ");
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("\nConexiune închisă brusc: " + e.Message);
+        }
+    }
+
+    public static void ReadMessageFromClient(NetworkStream stream)
+    {
+        try
+        {
+            while (true)
+            {
+                Console.Write("Introduceti mesajul: ");
+                string message = Console.ReadLine();
+                if (message != null)
+                {
+                    if (message[0] != '#')
+                    {
+                        byte[] data = Encoding.ASCII.GetBytes(message);
+                        stream.Write(data, 0, data.Length);
+                    }
+                    else 
+                    { 
+                        switch (message)
+                        {
+                            case "#exit":
+                                Environment.Exit(0);
+                                break;
+                            case "#decc":
+                                stream.Close();
+
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("A aparut o eroare" + e.StackTrace);
+        }
+    }
+
+    public static string GetMachineIP()
+    {
+        string hostName = Dns.GetHostName();
+        IPAddress[] localIPs = Dns.GetHostAddresses(hostName);
+        foreach (IPAddress ip in localIPs)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork) // Verifică dacă este adresă IPv4
+            {
+                return ip.ToString();
+            }
+        }
+        return "";
+    }
+
+    public Client(string nume)
+    {
+        Nume = nume;
+        IP = GetMachineIP();
+    }
+
+    public string getNume() { return Nume;}
+    public string getIP() { return IP; }
+}
---
+++ ServerClient/Server/Server.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Server
+{
+    private static List<TcpClient> clients = new List<TcpClient>();
+
+    public static void Main()
+    {
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        int port = 8080;
+        TcpListener listener = new TcpListener(ipAddress, port);
+        listener.Start();
+
+        Console.WriteLine("Serverul este pornit...");
+
+        Thread consoleThread = new Thread(ReadConsoleAndBroadcast);
+        consoleThread.Start();
+
+        while (true)
+        {
+            TcpClient client = listener.AcceptTcpClient();
+            clients.Add(client);
+            Console.WriteLine("Client conectat. :)");
+
+            Thread clientThread = new Thread(HandleClient);
+            clientThread.Start(client);
+        }
+    }
+
+    public static void HandleClient(object obj)
+    {
+        TcpClient client = (TcpClient)obj;
+        NetworkStream stream = client.GetStream();
+
+        try
+        {
+            while (true)
+            {
+                byte[] data = new byte[256];
+                int bytes = stream.Read(data, 0, data.Length);
+                string message = Encoding.ASCII.GetString(data, 0, bytes);
+                if (message[0] != '#')
+                {
+                    Console.WriteLine("Client: " + message);
+                    Console.Write("Server: ");
+                    BroadcastMessage2(message, client);
+                }
+                else if (message.Equals("#exit"))
+                    {
+                        clients.Remove(client);
+                        Console.WriteLine("client deconectat :(");
+                        break;
+                    }
+                    
+                
+            }
+        }
+        catch(IOException e)
+        {
+            Console.WriteLine("client deconectat :(");
+            clients.Remove(client);
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine("A aparut o eroare " + e.StackTrace);
+        }
+
+        client.Close();
+    }
+
+    public static void BroadcastMessage(string message) //Transmite un mesaj catre toti clientii
+    {
+        for (int i = clients.Count - 1; i >= 0; i--)
+        {
+            TcpClient client = clients[i];
+            if (client.Connected)
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] data = Encoding.ASCII.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+            }
+            else
+            {
+                clients.RemoveAt(i);
+            }
+        }
+    }
+
+    public static void ReadConsoleAndBroadcast()
+    {
+        retryReadingClient:
+        try 
+        {
+            while (true)
+            {
+                Console.Write("Server: ");
+                string message = Console.ReadLine();
+                if (message != null) 
+                { 
+                    if (message[0] != '#')
+                    {
+                        BroadcastMessage("" + message);
+                    }
+                    else HandleCommand(message); 
+                }
+            } 
+        }
+        catch (IndexOutOfRangeException e)
+        {
+            Console.WriteLine("Nu s-a putut transmite mesajul" + e.StackTrace);
+            goto retryReadingClient;
+        }
+    }
+
+    public static void BroadcastMessage2(string message, TcpClient client)  //Transmite mesajul unui client mai departe
+    {
+        for (int i = clients.Count - 1; i >= 0; i--)
+        {
+            TcpClient clientAux = clients[i];
+            if (clientAux.Connected && clientAux != client)
+            {
+                NetworkStream stream = clientAux.GetStream();
+                byte[] data = Encoding.ASCII.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+            }
+            else
+            {
+                clients.RemoveAt(i);
+            }
+        }
+    }
+
+    public static void HandleCommand(string mes)
+    {
+        switch(mes)
+        {
+            case "#close":                                                      //inchide consola si programul
+                Environment.Exit(0);
+                break;
+            case "#ccount":                                                     //afiseaza numarul de clienti conectati
+                Console.WriteLine("Numarul de clienti: " + clients.Count);
+                break;
+            case "#decon":                                                      //deconecteaza toti clientii                                                            
+                for (int i = clients.Count - 1; i >= 0; i--) 
+                { 
+                    clients[i].Dispose();
+                    clients.RemoveAt(i);
+                }
+                if (clients.Count == 0) Console.WriteLine("toti clientii au fost deconectati cu succes");
+                else Console.WriteLine("a aparut o eroare la deconectarea clientilor");
+                break;
+            default:
+                Console.WriteLine("comanda invalida");
+                break;
+        }
+    }
+}
